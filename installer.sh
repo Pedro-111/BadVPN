0b787f6ec9e7501da6c747f55238a15054ad7af0
Pedro-111 <108990464+Pedro-111@users.noreply.github.com>
2024-03-23 16:11:09 -0500

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,16 +1,23 @@
 #!/bin/bash
 
-# Descarga el script de GitHub
-wget https://raw.githubusercontent.com/usuario/repositorio/rama/nombre_del_script.sh -O /ruta/al/script/menu.sh
+# Crea el directorio si no existe
+mkdir -p /root/
 
-# Dale permisos de ejecución
-chmod +x /ruta/al/script/menu.sh
+# Descarga el script de GitHub
+wget https://raw.githubusercontent.com/Pedro-111/BadVPN/main/Main_BadVPN.sh?token=GHSAT0AAAAAACOPMOLU54OFYB65J374T6C6ZP7H36A -O /root/Main_BadVPN.sh
+
+# Dale permisos de ejecución al script
+chmod +x /root/Main_BadVPN.sh
+
+# Ejecuta el script
+/root/Main_BadVPN.sh
 
 # Añade un alias al archivo .bashrc
-echo "alias menu='/ruta/al/script/menu.sh'" >> ~/.bashrc
+echo "alias menu='/root/Main_BadVPN.sh'" >> ~/.bashrc
 
 # Recarga el archivo .bashrc
 source ~/.bashrc
 
 # Muestra un mensaje al usuario
 echo "El script se ha instalado correctamente. Puedes ejecutarlo con la palabra 'menu'."
+
